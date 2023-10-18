65f796d7e3e109123df1c88fa383753fd46f22fb
Cormac Costello <cormaccostello2@gmail.com>
2023-10-18 21:44:19 +0100

    include search menu options in optionSearch.sh

---
+++ BashCatalog/optionSearch.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+# Author: Cormac Costello
+# Description: display the options in the search menu
+read word
+case $word in
+	[yY] | [yY][Ee][Ss] )
+	echo "OK"
+	;;
+	[nN] | [Nn][Oo] )
+        echo "You chose No,";
+        break 2 
+        ;;
+        *) echo "Invalid input"
+        ;;
+        esac
+        echo
+echo "1) CatalogID"
+echo "2) Name"
+echo "3) Album"
+echo "4) Genre"
+echo "5) Exit"
