b18aaa83cf098dcf9ddbf135aba6b12f0cf83e02
Cormac Costello <cormaccostello2@gmail.com>
2023-10-19 23:23:13 +0100

    copied all to new catalog to (begin to) fix some errors in csv format output and parsing csv

---
+++ Catalog/addRecord.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+# Author: Cormac Costello
+# Script that can be used to add a new music track to musictracks.csv
+echo "Welcome $USER, get ready to add a new entry"
+catalog_file="musictracks.csv"
+if [ -a "$catalog_file" ]; then
+	echo "Hooray! "
+	echo
+	echo "catalogue file exists"
+else
+	echo "Creating new catalogue"
+    	touch "$catalog_file"
+	echo "\"Catalog Number\",\"Track Name\",\"Artist\",\"Genre\",\"Duration\"" >> "$catalog_file"
+fi
+
+while true; do
+
+padNumber() {
+	printf "%03d" "$1"
+}
+
+#find the last catalog number and increment it
+#Note: sed is used to ignore the leading zero's so the catalog number is treated as decimal rather than octal
+numLines=`wc -l musictracks.csv | awk '{print $1}'`
+if [ $numLines -gt 1 ]; then
+	last_cat_number=$(tail -n 1 "$catalog_file" | awk 'BEGIN {FS=","}{gsub(/"/, "", $1); print $1}' | sed 's/^0*//')
+else
+	last_cat_number=0
+fi
+
+next_cat_number=$((last_cat_number + 1))
+
+#pad the number to three digits
+next_cat_num_pad=$(padNumber "$next_cat_number")
+
+echo "Your new track will have auto-generated catalog number of: $next_cat_num_pad"
+
+read -p "Enter Track Name: " trackName
+
+read -p "Enter Artist: " artist
+
+read -p "Enter Album: " album
+
+read -p "Enter Genre: " genre
+
+validate_input() {
+	local input="$1"
+	if [[ "$input" =~ ^[0-9]+$ && "$input" -ge 0 && "$input" -le 59 && ${#input} -le 2 ]]; then
+		return 0 # valid input
+	else
+		return 1 #invalid input
+	fi
+}
+
+echo "Enter Duration"
+# read and validate minutes
+while true; do
+
+	read -p "minutes: " minutes
+	if validate_input "$minutes"; then
+		break #valid input, exit loop
+	else
+		echo "Invalid input. Please enter a number between 0 and 59"
+	fi
+done
+
+# read and validate seconds
+while true; do
+	read -p "seconds: " seconds
+	if validate_input "$seconds"; then
+		break
+	else
+		echo "Invalid input. Please enter a number between 0 and 59"
+	fi
+done
+
+padMinutes=`printf "%02d" "$minutes"`
+padSeconds=`printf "%02d" "$seconds"`
+
+duration=`printf "$padMinutes:$padSeconds"`
+
+echo "\"$next_cat_num_pad\",\"$trackName\",\"$artist\",\"$album\",\"$genre\",\"$duration\"" >> "musictracks.csv"
+echo "Added successfully"
+echo
+echo "The Track you just added is:"
+echo
+head -n 1 musictracks.csv | sed 's/,/ /g' #print the column headers using sed
+echo
+echo "Catalog Number:  $next_cat_num_pad"
+echo "Track Name: $trackName"
+echo "Artist: $artist"
+echo "Album: $album"
+echo "Genre: $genre"
+echo "Duration: $duration"
+echo
+echo
+awk 'BEGIN {FS=","}{gsub(/"/, "", $0); print $0}' musictracks.csv 
+echo
+echo
+echo "Would you like to add another track? (y/n) "
+read choice
+case $choice in
+
+        [yY] | [yY][eE][sS] )
+                echo "Agreed"
+                ;;
+        [nN] | [nN][oO] )
+                echo "You chose No"
+		sleep 1
+		echo ---------------------------------------------------------------------
+		echo "Returning to Main Menu"
+		echo
+		sleep 1
+		echo "1) Add"
+		echo "2) Search"
+		echo "3) Remove"
+		echo "4) Generate Report"
+		echo "5) Quit"
+                break
+                ;;
+        *) echo "Invalid Input"
+                echo "Please try again"
+                ;;
+
+esac
+
+done
---
+++ Catalog/searchRecords.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+# Author: Cormac Costello. Student ID: 11399631
+# Script for computer systems continuous assessment homework week 4
+echo "Welcome $USER!"
+echo
+
+if [[ -e musictracks.csv ]]; # check if the employee.txt file exists and if not, create it
+        then echo "catalog file exists"
+        else touch "musictracks.csv" #creates a new employee.txt file
+fi
+numRec=`wc -l musictracks.csv | awk '{print $1}'` # finds number of employee records
+if [ "$numRec" -eq 1 ]  #checks if there are records, if there are not, it exits the program (no search records required for this assignment)
+        then echo "There are no records in our catalog"
+        echo
+        echo
+        echo "Would you like to add a new record? (y/n)" # here we might execute another program that allows user to create records, but not in this exercise
+        echo
+        read choice
+            case $choice in
+
+                [yY] | [yY][eE][sS] )
+                        echo "Adding new music track"
+                        /bin/bash addRecord.sh
+                        ;;
+                [nN] | [nN][oO] )
+                        echo "Not agreed"
+                        break
+                        ;;
+                *) echo "Invalid Input"
+                        echo "Please try again"
+                        ;;
+            esac
+        exit 0
+else
+        while [ "$numRec" -ge 1 ] # enters a loop that executes until the user chooses to exit
+        do
+                echo "Here is the full list of records:"
+                echo
+                cat musictracks.csv
+                echo
+                echo ----------------------------------------------------------------------
+                echo
+                echo
+                echo "Would you like to search for a track by 1) catalogID 2) name 3) album 4) genre or 5) exit the program?" 
+                echo
+                                        #continue editing from here
+                PS3="Please choose an option (must be a number) "
+                select opt in CatalogID Name Album Genre Exit
+                do
+                        case $opt in
+                                "CatalogID")
+                                        echo "Please Enter the catalog number of the track that you wish to search for:"
+                                        echo 
+                                        echo "Must be a three digit number"
+                                        read trackNum
+                                        while ! [[ "$trackNum" =~ ^[0-9]{3}$ ]]; do
+                                            echo "Invalid entry \"$trackNum\", please enter a three-digit number"
+                                            read trackNum
+                                        done
+                                        trackNumExists=`grep $trackNum musictracks.csv | wc -l`
+                                            if [ "$trackNumExists" -eq 0 ] 
+                                                        then echo "This Track Number \"$trackNum\" does not exist in our records"
+                                                        echo
+                                                        echo "Please try again"
+                                                        echo
+                                                        echo --------------------------------------------------------
+                                                        echo
+                                                else
+                                                        echo "There are $trackNumExists tracks matching your search: "
+                                                        grep $trackNum musictracks.csv | sed 's/;/ /g' 2> /dev/null
+                                                        echo
+                                                        echo "Would you like to search again? (y/n)"
+                                                        echo
+							/bin/bash optionSearch.sh
+                                            fi
+                                                        ;;
+
+                                "Name" )
+                                        echo "Please Enter the name of the track that you wish to search for:"
+                                        echo
+                                        read trackName
+                                        nameExists=`grep -i $trackName musictracks.csv | wc -l`
+                                                if [ "$nameExists" -eq 0 ]
+                                                        then echo "This track \"$trackName\" does not exist in our records"
+                                                        echo
+                                                        echo "Please try again"
+                                                        echo
+                                                        echo --------------------------------------------------------
+                                                        echo
+                                                else
+                                                        echo "There are $nameExists tracks matching your search: "
+                                                        echo
+                                                        grep -i "$trackName" musictracks.csv | sed 's/;/ /g' 2> /dev/null
+                                                fi
+                                        echo
+                                        echo "Would you like to search again? (y/n)"
+                                        echo
+                                        echo
+                                        sleep 1
+                                        echo
+					/bin/bash optionSearch.sh
+                                        ;;
+                                "Album" )
+                                        echo "Please Enter the album that you wish to search for:"
+                                        echo
+                                        read album
+                                        albumExists=`grep -i $album musictracks.csv | wc -l`
+                                                if [ "$albumExists" -eq 0 ]
+                                                        then echo "This album \"$album\" does not exist in our records"
+                                                        echo
+                                                        echo "Please try again"
+                                                        echo
+                                                        echo --------------------------------------------------------
+                                                        echo
+                                                else
+                                                        echo "There are $nameExists tracks matching your search: "
+                                                        echo
+                                                        grep -i "$album" musictracks.csv | sed 's/;/ /g' 2> /dev/null
+                                                fi
+                                        echo
+                                        echo "Would you like to search again? (y/n)"
+                                        echo
+                                        echo
+                                        sleep 1
+                                        echo
+                                        ;;
+                                "Genre" )
+                                        echo "Please Enter the genre that you wish to search for:"
+                                        echo
+                                        read genre
+                                        genreExists=`grep -i $genre musictracks.csv | wc -l`
+                                                if [ "$genreExists" -eq 0 ]
+                                                        then echo "This genre \"$genre\" does not exist in our records"
+                                                        echo
+                                                        echo "Please try again"
+                                                        echo
+                                                        echo --------------------------------------------------------
+                                                        echo
+                                                else
+                                                        echo "There are $genreExists tracks matching your search: "
+							echo
+                                                        grep -i "$genre" musictracks.csv | sed 's/;/ /g' 2> /dev/null
+                                                fi
+                                        echo
+                                        echo "Would you like to search again? (y/n)"
+                                        echo
+                                        echo
+                                        sleep 1
+                                        echo
+					/bin/bash optionSearch.sh
+                                        ;;
+                                "Exit" )
+                                        echo "You chose exit"
+                                        sleep 1
+                                        echo
+                                        echo ---------------------------------------------------------------------
+                			echo "Returning to Main Menu"
+                			echo
+                			sleep 1
+                			echo "1) Add"
+               				echo "2) Search"
+                			echo "3) Remove"
+                			echo "4) Generate Report"
+                			echo "5) Quit"
+                                        exit 0
+                                        ;;
+                                *)
+                                        echo "Invalid option, Please try again"
+                                        echo
+                                        sleep 1
+                                        echo
+                                        echo
+                                        ;;
+                        esac
+                done
+        done
+fi
